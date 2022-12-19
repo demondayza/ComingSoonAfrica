717cb46a21dba72ba950c374b43690981fea8b5b
Andrew Margetts <demondayza@users.noreply.github.com>
2022-12-19 12:28:26 +0200

    changes for quick deploy

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,19 +9,19 @@ type Props = {
 const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div className="relative min-h-screen flex flex-col justify-center items-center bg-hero bg-cover">
     <Head>
-      <title>{title} | Themeptation</title>
+      <title>{title} | Africa.validator n</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta
         name="description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
+        content="Africa.validator - The first truly African Validator service"
       />
       <meta property="og:title" content={`${title} | Themeptation`} />
       <meta
         property="og:description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
+        content="Africa.validator - The first truly African Validator service"
       />
-      <meta property="og:url" content="https://slioth.themepttation.net/" />
+      <meta property="og:url" content="https://northroom.digital/" />
       <meta property="og:type" content="website" />
     </Head>
     {children}
